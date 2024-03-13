014fd93dd3f74420d5794336078287251c12fa5c
Vlad Kuznetsov <ghost.vkv@gmail.com>
2024-03-13 21:18:36 +0300

    auth middleware

--- handler_user.go
+++ handler_user.go
@@ -38,21 +38,6 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	respondWithJSON(w, 201, databaseUserToUser(dbUser))
 }
 
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
-	apiKey, err := auth.GetAPIKey(r.Header)
-	if err != nil {
-		respondWithError(w, 403, "forbidden")
-		return
-	}
-
-	// Context() позволяет следить за стейтом нескольких goroutines
-	// Важная функция - это отмена действий, например, дает возможность
-	// закончить http request
-	dbUser, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
-	if err != nil {
-		respondWithError(w, 403, fmt.Sprintf("Couldn't get user info: %v", err))
-		return
-	}
-
-	respondWithJSON(w, 200, databaseUserToUser(dbUser))
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
+	respondWithJSON(w, 200, databaseUserToUser(user))
 }
